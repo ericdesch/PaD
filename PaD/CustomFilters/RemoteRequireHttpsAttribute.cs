9c3e78f0490d55af56357de010e570bac3c25d7d
Eric Desch <ericdesch@outlook.com>
2017-02-05 21:09:52 -0800

    Make RemoteRequireHttpsAttribute more robust and configurable from appSettings. Add comment to do this as a rewriterule in the future.

--- PaD/CustomFilters/RemoteRequireHttpsAttribute.cs
+++ PaD/CustomFilters/RemoteRequireHttpsAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,19 @@ namespace PaD.CustomFilters
 {
     public class RemoteRequireHttpsAttribute : RequireHttpsAttribute
     {
+        //TODO: do this as a rewrite rule. Change enabled="false" to "true" when deploying
+        //<rewrite>
+        //  <rules>
+        //    <rule name = "SSL_ENABLED" enabled="false" stopProcessing="true">
+        //      <match url = "(.*)" />
+        //      < conditions >
+        //        < add input="{HTTPS}" pattern="^OFF$" />
+        //      </conditions>
+        //      <action type = "Redirect" url="https://{HTTP_HOST}/{R:1}" appendQueryString="true" redirectType="Permanent" />
+        //    </rule>
+        //  </rules>
+        //</rewrite>
+
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
             if (filterContext == null)
@@ -15,9 +29,35 @@ namespace PaD.CustomFilters
                 throw new ArgumentNullException("RemoteRequireHttpsAttribute: filterContext is null");
             }
 
-            if (filterContext.HttpContext != null && filterContext.HttpContext.Request.IsLocal)
+            if (filterContext.HttpContext != null)
             {
-                return;
+                if (filterContext.HttpContext.Request.IsSecureConnection)
+                {
+                    return;
+                }
+
+                var currentUrl = filterContext.HttpContext.Request.Url;
+                if (currentUrl.Scheme.Equals(Uri.UriSchemeHttps, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    return;
+                }
+
+                if (string.Equals(filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"], "https", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return;
+                }
+
+                if (filterContext.HttpContext.Request.IsLocal)
+                {
+                    return;
+                }
+
+                var val = ConfigurationManager.AppSettings["RequireSSL"].Trim();
+                var requireSsl = bool.Parse(val);
+                if (!requireSsl)
+                {
+                    return;
+                }
             }
 
             base.OnAuthorization(filterContext);
